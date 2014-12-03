7d8d6c3ef25435c63a1a5c373128ad9b37536dbb
Hannah S Douglas <h.s.douglas@gmail.com>
2014-12-02 21:11:43 -0500

    Base rails app

---
+++ HackerNewsViewer/config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+HackerNewsViewer::Application.config.secret_key_base = '90f1041deb11d7e42508c78e10d1549079c9af898242710291525c253b6e1b8e10517331ce7d8334b5468c81c2325712c3c93f2af12eb06730a3211823e59d22'
---
+++ interview/config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Interview::Application.config.secret_key_base = 'ca7f14b9dae85e8b98c8be33607d49d48d88c38d3b2f41d9948d5992d8c6e91e0eb44eae0893d35c2df07c9c8575547a6b0cc28020085bc58b201c23b58266dd'
